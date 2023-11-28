43b00f2ac681a2d9e7fcd6e0a46d21dbbd25df61
KennyThrug <kennyepstein6@gmail.com>
2023-11-28 11:32:25 -0600

    Changing a hardcoded array to a map

--- src/Pages/PostPage.js
+++ src/Pages/PostPage.js
@@ -1,14 +1,19 @@
 
 export function renderPostPage() {
-    let post = placeholder_posts()[0];
+    //Change to a get from the database API
+    let post = placeholder_posts();
     return (
         <div>
-            <div style={{ border: "solid"}}>
-                Post Title: {post.postTitle}
-            </div>
-            <div>
-                Post Contents: {post.postContents}
-            </div>
+            {post.map((cur_post, index) => (
+                <div>
+                    <div style={{ border: "solid" }}>
+                        Post Title: {cur_post.postTitle}
+                    </div>
+                    <div>
+                        Post Contents: {cur_post.postContents}
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
@@ -26,11 +31,27 @@ function placeholder_posts() {
             ],
             postContents: (
                 <div>
-                    This is a post that is really long, and is not actually real. I don't know what to do here. Ahhhhhhhhh
+                    This is a post that is really long, and is not actually real. I don't know what to do here. Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
                     <img src="https://robohash.org/Kenny"></img>
-                    That was an Inline Image, I hope it works
+                    That was an Inline Image, I hope it works  Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
                 </div>
             )
-        }
+        },
+        {
+            id: 0,
+            postTitle: "Placeholder Post Title",
+            tags: [
+                "Fantasy",
+                "TestPost",
+                "Dragons"
+            ],
+            postContents: (
+                <div>
+                    This is a second post, which should show up
+                    <img style={{float: "left"}} src="https://robohash.org/Ked"></img>
+                    That was an Inline Image, It seems to work
+                </div>
+            )
+        },
     ]
 }
