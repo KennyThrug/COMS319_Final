2c563fa51413e01b101f21971bc86555897c53c3
KennyThrug <kennyepstein6@gmail.com>
2023-12-01 17:19:41 -0600

    Post will now have author and dates

--- src/Pages/PostPage.js
+++ src/Pages/PostPage.js
@@ -3,17 +3,8 @@ export function renderAllPostPage(posts) {
     return (
         <div>
             {posts.map((cur_post, index) => (
-                <div style={{ marginLeft: "30%", marginRight: "30%"}}>
-                    <div style={{ border: "solid" }}>
-                        <h1>
-                            Post Title: {cur_post.postTitle}
-                        </h1>
-                    </div>
-                    <div>
-                        <p>
-                            Post Contents: {cur_post.postContents}
-                        </p>
-                    </div>
+                <div>
+                    {renderSinglePost(cur_post)}
                 </div>
             ))}
         </div>
@@ -23,16 +14,28 @@ export function renderAllPostPage(posts) {
 //Given an array of posts, and an index, it should render that post
 export function renderSinglePostFromArray(posts, index) {
     return renderSinglePost(posts[index]);
+    //possibly a way to render comments as well here
 }
 
 //Given a single post, render it
 export function renderSinglePost(post){
     return (
         <div style={{ marginLeft: "30%", marginRight: "30%"}}>
-            <div style={{ border: "solid" }}>
+            <div style={{ border: "solid" }} className="columns-2">
                 <h1>
                     Post Title: {post.postTitle}
                 </h1>
+                <h2>
+                    Author: {post.author}
+                </h2>
+            </div>
+            <div className="columns-2">
+                <h2>
+                    Date Published: {post.date_published}
+                </h2>
+                <h3>
+                    Date of Post: {post.date}
+                </h3>
             </div>
             <div>
                 <p>
